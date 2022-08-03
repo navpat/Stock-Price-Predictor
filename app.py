58329e26efbf1c551415a4bbb3e937005884c466
navpat <nav.pathak18@gmail.com>
2022-08-03 11:02:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,131 @@
+from xmlrpc.client import DateTime
+import dash
+from dash import dcc
+from dash import html
+from datetime import date
+import yfinance as yf
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
+from dash.dependencies import Input,Output,State
+import model
+
+app=dash.Dash(__name__)
+server=app.server
+
+item1=html.Div(
+            [
+                html.P("Welcome to the Stock Dash App",className="start"),
+                html.Div([
+                    html.P("Input Stock Code"),
+                    dcc.Input(type="text",value='',id="short-stock-name"),
+                    html.Button("Submit",name='Submit',n_clicks=0,id="Submit")
+                ],className="stock"),
+                html.Div([
+                    dcc.DatePickerRange(min_date_allowed=date(1975,1,1),max_date_allowed=date.today(),initial_visible_month=date.today(),end_date=date.today(),id="datepicker")
+                ]),
+                html.Div([
+                    html.Button("Stock Price",name='Stock Price',n_clicks=0,className="btn",id="price"),
+                    html.Button("Indicators",name='Indicators',n_clicks=0,className="btn",id="EMA"),
+                    dcc.Input(type="text",value='',placeholder='number of days',className="days",id="n_days"),
+                    html.Button("Forecast",name='Forecast',n_clicks=0,className="btn",id="n_days_btn")
+                ])
+            ],
+            className="nav")
+item2=html.Div(
+            [
+                html.Div([
+                    #LOGO
+                    #COMPANY NAME
+
+                ],className="header",id="header"),
+                html.Div( #DESCRIPTION
+                id="description",className="description_ticker"
+                ),
+                html.Div([
+                    dcc.Graph(id="graph1")#STOCK PRICE PLOT
+
+                ],id="graphs-content"),
+                html.Div([
+                    dcc.Graph(id="graph2")#INDICATOR PLOT
+
+                ],id="main-content"),
+                html.Div([
+                    dcc.Graph(id="graph3")#FORECAST PLOT
+
+                ],id="forecast-content")
+
+            ],className="Content")
+
+app.layout=html.Div([item1,item2],className="container")
+@app.callback(
+    [Output(component_id="header",component_property="children"),Output(component_id="description",component_property="children")],
+    [Input(component_id="Submit",component_property="n_clicks")],
+    [State(component_id="short-stock-name",component_property="value")]
+)
+def update_data(n,val):
+    ticker=yf.Ticker(val)
+    inf=ticker.info
+    df=pd.DataFrame().from_dict(inf,orient="index")
+    return df[0]["shortName"],df[0][["longBusinessSummary"]]
+
+@app.callback(
+    Output("graph1","figure"),
+    [Input(component_id="price",component_property="n_clicks")],
+    [State(component_id="datepicker",component_property="start_date"),State(component_id="datepicker",component_property="end_date"),State(component_id="short-stock-name",component_property="value")]
+
+)
+def plot_graph(n,start_date,end_date,val):
+    df=yf.download(val,start=start_date,end=end_date)
+    df.reset_index(inplace=True)
+    df1=df[["Date","Open","Close"]].copy()
+    print(df1)
+    fig=get_stock_price_fig(df1)
+    return fig
+
+def get_stock_price_fig(df):
+    fig=px.line(df,x="Date",y=["Open","Close"],title="Closing and Opening Price vs Date")
+    return fig
+
+@app.callback(
+    Output("graph2","figure"),
+    [Input(component_id="EMA",component_property="n_clicks")],
+    [State(component_id="datepicker",component_property="start_date"),State(component_id="datepicker",component_property="end_date"),State(component_id="short-stock-name",component_property="value")]
+
+)
+def plot_graph(n,start_date,end_date,val):
+    df=yf.download(val,start=start_date,end=end_date)
+    df.reset_index(inplace=True)
+    df1=df[["Date","Open","Close"]].copy()
+    print(df1)
+    fig=get_more(df1)
+    return fig
+
+def get_more(df):
+    df['EWA_20']=df['Close'].ewm(span=20, adjust=False).mean()
+    fig=px.scatter(df,x="Date",y="EWA_20")
+    return fig
+
+@app.callback(
+    Output('graph3','figure'),
+    [Input(component_id="n_days_btn",component_property="n_clicks")],
+    [State(component_id="n_days",component_property="value"),State(component_id="short-stock-name",component_property="value")]
+)
+
+def plot_predictor_graph(n,n_days,val):
+    today=date.today()
+    print(type(n_days))
+    dates=pd.date_range(today,periods=int(n_days)).tolist()
+    predicted_price=model.train_model(val,today,int(n_days))
+    data={'Dates':dates,'Price':predicted_price}
+    df=pd.DataFrame(data)
+    print(df)
+    fig=plot_points(df)
+    return fig
+
+def plot_points(df):
+    fig=px.line(df,x="Dates",y="Price")
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ model.py
@@ -0,0 +1,68 @@
+import math
+import yfinance as yf
+import numpy as np
+from sklearn.svm import SVR
+from sklearn.model_selection import train_test_split,GridSearchCV,cross_validate
+from sklearn.utils import shuffle
+from datetime import date
+
+
+def train_model(val,end_date,n_days_predict):
+    df=yf.download(val,start="2020-01-01",end=end_date)
+
+    df1=df[["Adj Close"]]
+    df1["Prediction"]=df[["Adj Close"]].shift(-n_days_predict)
+
+    X=np.array(df1.drop(['Prediction'],1)) #create independent data
+    X=X[:-n_days_predict]
+
+    Y=np.array(df1["Prediction"]) #create dependent data
+    Y=Y[:-n_days_predict]
+
+    X_forecast=np.array(df1.drop(['Prediction'],1))[-n_days_predict:]
+
+    #x_train,y_train,x_test,y_test=train_test_split(X,Y,test_size=0.1)  #splitting the data
+    seed = 42
+    X, Y = shuffle(X, Y, random_state=seed)
+    gsc=GridSearchCV(
+        estimator=SVR(kernel='rbf'),
+        param_grid={
+            'C' :[0.1,1,100,1000],
+            'epsilon' :[0.0001,0.0005,0.001,0.005,0.01,0.05,0.1,0.5,1,5,10],
+            'gamma' :[0.0001,0.001,0.005,0.1,1,3,5]
+        },
+        cv=5,scoring='neg_mean_squared_error',verbose=0,n_jobs=-1
+    )
+    grid_result=gsc.fit(X,Y)
+    best_params=grid_result.best_params_
+    best_svr = SVR(kernel='rbf', C=best_params["C"], epsilon=best_params["epsilon"], gamma=best_params["gamma"],
+                   coef0=0.1, shrinking=True,
+                   tol=0.001, cache_size=200, verbose=False, max_iter=-1)
+    rbf_svr=best_svr
+    rbf_svr.fit(X,Y)
+    scoring = {
+               'abs_error': 'neg_mean_absolute_error',
+               'squared_error': 'neg_mean_squared_error'}
+    scores=cross_validate(best_svr,X,Y, cv=10,scoring=scoring,return_train_score=True)
+    svm_prediction=rbf_svr.predict(X_forecast)
+    return svm_prediction.tolist()
+    #return "MAE :", abs(scores['test_abs_error'].mean()), "| RMSE :", math.sqrt(abs(scores['test_squared_error'].mean())),"| MAPE :", my_custom_MAPE(best_svr,X,Y,best_params["epsilon"])
+
+def my_custom_MAPE(clf, X_val, y_true,epsilon):
+      y_pred = clf.predict(X_val)
+      ii = 0
+      for i in y_true:
+          if (i < epsilon) & (i > -epsilon):
+             y_true[ii] = epsilon
+          else:
+             y_true[ii] = y_true[ii]
+          ii = ii+1
+      MAPE = (1/len(y_true))*np.sum(np.abs(y_true - y_pred)/y_true)
+      return MAPE
+
+
+#today=date.today()
+#d1=today.strftime("%Y-%m-%d")
+#print(d1)
+#print(train_model("AAPL",d1,20))
+
